360bd65a8cd5286c5c366645746d1570d6073ba1
saboorahmedkhan <saboorahmedkhan@gmail.com>
2023-10-10 00:01:26 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className='text-[32px]'>Vercel Postrgress database</h1>
+    </main>
+  )
+}
